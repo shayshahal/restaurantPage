efa8734073fabc4a30c72fb7002f950388dbc678
shayshahal <shay840@gmail.com>
2022-11-25 21:02:44 +0200

    Blur background

--- src/tab1.js
+++ src/tab1.js
@@ -3,12 +3,18 @@ import bg from './assets/Baratie.jpg';
 export default function loadTab(tab)
 {
     // Change background for tab
-    const main = document.getElementById("main-container");
-    main.style.backgroundImage = `url(${bg})`;
+    const content = document.getElementById("content");
+    content.style.backgroundImage = `url(${bg})`;
 
+    // Create blur for background
+    const main = document.getElementById("main-container");
+    main.style.backdropFilter = 'blur(3px)';
+    
     // Tab attributes
     tab.id = "tab1";
     tab.classList.add('tab');
+
+
 
     // Create Welcome message!
     const welcome = document.createElement('h1');
