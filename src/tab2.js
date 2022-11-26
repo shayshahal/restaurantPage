e3cf5c30741123957fcaf379c22f9e0496e01f15
shayshahal <shay840@gmail.com>
2022-11-26 03:55:30 +0200

    Menu Tab 2

--- src/tab2.js
+++ src/tab2.js
@@ -1,12 +1,51 @@
+import bg from './assets/Pizza.jpg';
+import italian from './assets/italian.jpg';
+
 export default function loadTab(tab)
 {
+        // Change background for tab
+        const content = document.getElementById("content");
+        content.style.backgroundImage = `url(${bg})`;
+    
+        // Create blur for background
+        const main = document.getElementById("main-container");
+        main.style.backdropFilter = 'blur(7px)';
+        
     // Tab attributes
     tab.id = "tab2";
     tab.classList.add('tab');
 
-    // Create Welcome message!
-    const welcome = document.createElement('div');
-    welcome.textContent = 'hahaaaaaaaaaaaaa';
-    tab.appendChild(welcome);
+    // Create Menu message!
+    const title = document.createElement('div');
+    title.textContent = 'Menu:';
+    tab.appendChild(title);
+
+    const list = document.createElement('div');
+    list.classList.add("menu-list");
+    tab.appendChild(list);
+    for (let i = 0; i < 3; i++) 
+    {
+        const menuItem = document.createElement('div');
+        menuItem.classList.add("menu-item");
+        list.appendChild(menuItem);
+
+        const itemImg = new Image();
+        itemImg.src = italian;
+        menuItem.appendChild(itemImg);
+
+        const wordsDiv = document.createElement("div");
+        wordsDiv.classList.add('item-words');
+        menuItem.appendChild(wordsDiv);
+
+        const name = document.createElement("p");
+        name.textContent = "Italian:";
+        name.classList.add('item-name');
+        wordsDiv.appendChild(name);
+
+        const desc = document.createElement("p");
+        desc.textContent = "asdmalsdmladsmlad,mlasd,amdlasdlad, askdjaoskdjaosdkajdoaksdj";
+        desc.classList.add('item-desc');
+        wordsDiv.appendChild(desc);
+    }
 
 }
